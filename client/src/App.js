ffe0ef818d0a4de8db1aba69230d4ccacab7e64e
Mariano <mariano_farace@hotmail.com>
2022-01-17 19:41:53 +0100

    Signup frontend form implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {  BrowserRouter,  Routes,  Route,} from "react-router-dom";
 import Navbar from './components/layout/Navbar';
+import Signup from './components/auth/Signup';
+import { UserContext } from './UserContext';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
+      <UserContext.Provider value={{ user, setUser }}>
           <Navbar />
-           </div>
-    </Router>
-  );;
+          <Routes>
+            <Route path="/signup" element= {<Signup />} />
+          </Routes>
+          </UserContext.Provider>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
