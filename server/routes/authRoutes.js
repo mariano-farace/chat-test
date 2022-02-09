d4620b11b0d0ad250cac74703c18d08f6850ec17
Mariano <mariano_farace@hotmail.com>
2022-02-09 14:01:29 +0100

    Add login route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,5 +3,6 @@ const authController = require('../controllers/authControllers');
 
 const router = Router();
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 module.exports = router;
