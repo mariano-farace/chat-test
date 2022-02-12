f7eb0f987e15ed4f81fe2036f88258c576dc0efd
Mariano <mariano_farace@hotmail.com>
2022-02-12 12:10:12 +0100

    Implement join room

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,11 +1,28 @@
 import React, { useContext, useEffect, useState } from "react"
-
+import io from "socket.io-client"
+import { useParams } from "react-router-dom"
 import { UserContext } from "../../UserContext"
 import Input from "./Input"
 import Messages from "./Messages"
 
+let socket
+
 function Chat() {
+  const ENDPT = "localhost:5000"
+  const { user, setUser } = useContext(UserContext)
+  const { room_id, room_name } = useParams()
   const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    socket = io(ENDPT)
+    socket.emit("join", { name: user.name, room_id, user_id: user._id })
+  }, [])
+
+  // const sendMessage = (e) => {
+  //   e.preventDefault()
+  //   if (message) {
+  //   }
+  // }
 
   return (
     <div className="outerContainer">
