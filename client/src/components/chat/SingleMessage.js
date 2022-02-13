8f5782671f719c791c8128608ac2f9ceff65009f
Mariano <mariano_farace@hotmail.com>
2022-02-13 10:48:12 +0100

    Implement diffent message box for sender and receiber

--- client/src/components/chat/SingleMessage.js
+++ client/src/components/chat/SingleMessage.js
@@ -2,10 +2,24 @@ import React from "react"
 import "./SingleMessage.css"
 
 function SingleMessage({ message: { name, user_id, text }, current_uid }) {
-  return (
+  let isCurrentUser = false
+  if (user_id === current_uid) {
+    isCurrentUser = true
+  }
+  return isCurrentUser ? (
+    <div className="row right-align">
+      <div className="col s12 m8 16 right">
+        <p className="sentbyme">
+          {" "}
+          {name}: {text}
+        </p>
+      </div>
+    </div>
+  ) : (
     <div className="row left-align">
       <div className="col s12 m8 16 left">
         <p className="opponent">
+          {" "}
           {name}: {text}
         </p>
       </div>
