59f08de44f1bd67d76b624ba52d0e5235970255f
Mariano <mariano_farace@hotmail.com>
2022-02-15 12:46:01 +0100

    Remove comments

--- client/src/components/auth/Redirect.js
+++ client/src/components/auth/Redirect.js
@@ -31,22 +31,3 @@ function Redirect() {
 }
 
 export default Redirect
-
-// useEffect(() => {
-//   async function getMe() {
-//     await fetch("http://localhost:5000/google-login-redirect", {
-//       credentials: "include",
-//     }).then((res) => setUser(res.data))
-//   }
-
-//   getMe()
-// }, [])
-
-// const res = await fetch("http://localhost:5000/google-login-redirect", {
-//   method: "POST",
-//   credentials: "include",
-//   body: JSON.stringify({ name, email, password }),
-//   headers: { "Content-Type": "application/json" },
-// })
-// const data = await res.json() // TODO por que necesita await???????????' https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-// console.log("data!!!!!!", data)
