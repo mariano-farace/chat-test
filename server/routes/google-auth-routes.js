7d856c01673946ede447589b9c59f0a3e4439c54
Mariano <mariano_farace@hotmail.com>
2022-03-08 07:57:50 +0100

    Add minor comment and  Auth flow picture

--- server/routes/google-auth-routes.js
+++ server/routes/google-auth-routes.js
@@ -3,6 +3,7 @@ const googleAuthController = require('../controllers/google-auth-controllers');
 
 const router = Router();
 
+// Returns to google auth url for login
 router.get('/auth/google/url', googleAuthController.getGoogleAuthURL);
 router.get('/login/google-auth', googleAuthController.googleAuthCheckDbSendJWT);
 router.get('/google-login-redirect', googleAuthController.verifyGoogleAuthToken);
