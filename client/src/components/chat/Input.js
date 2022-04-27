4d019272eadc48100535cd16126bd2c477fc58f4
Mariano <mariano_farace@hotmail.com>
2022-04-27 09:15:46 +0200

    Style chat screen

--- client/src/components/chat/Input.js
+++ client/src/components/chat/Input.js
@@ -1,10 +1,13 @@
 import React from "react"
 import "./Input.css"
+import { IconButton } from "@mui/material"
+import SendIcon from "@mui/icons-material/Send"
 
 function Input({ message, setMessage, sendMessage }) {
   return (
     <form action="" onSubmit={sendMessage} className="form">
       <input
+        id="input2"
         type="text"
         className="input"
         placeholder="Type a message"
@@ -12,7 +15,26 @@ function Input({ message, setMessage, sendMessage }) {
         onChange={(e) => setMessage(e.target.value)}
         onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
       />
-      <button className="sendButton">Send</button>
+
+      <IconButton
+        onClick={sendMessage}
+        variant="contained"
+        color="primary"
+        aria-label="upload picture"
+        component="span"
+        size="small"
+      >
+        <SendIcon
+          style={{
+            color: "white",
+            background: "blue",
+            borderRadius: "80% ",
+            width: "40px",
+            height: "40px",
+          }}
+          fontSize="large"
+        />
+      </IconButton>
     </form>
   )
 }
